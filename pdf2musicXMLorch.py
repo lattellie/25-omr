1827eae131649a085672156580a02ea455e04795
lattellie <lattellie@gmail.com>
2026-03-02 01:10:39 -0800

    construct Bar by each track

--- pdf2musicXMLorch.py
+++ pdf2musicXMLorch.py
@@ -10,15 +10,116 @@ from utils import *
 
 
 
+
+class ScoreMetaData:
+    # static 
+    instruments: List[str] = []
+
+    def __init__(self, pd_frame: pd.DataFrame, json_path: str, initInstruments = False):
+        self.jsonPath: str = json_path
+        self.pdFrame: pd.DataFrame = pd_frame
+        self.trackRange: List[Tuple[int,int]] = []
+        self.groupingRange: List[List[Tuple[int,int]]] = []
+        if initInstruments:
+            for i in range(1,4):
+                df[f"combined{i}"] = (
+                    df[f"ins{i}"]
+                    + df[f"part{i}"].apply(lambda x: "" if pd.isna(x) else '_'+str(int(x)))
+                    + df[f"tone{i}"].apply(lambda x: "" if pd.isna(x) else ':'+x)
+                )
+            df_filtered = df[['combined1', 'combined2','combined3']][df['system'] == 1]
+            instrument_list = [str(x) for x in df_filtered.to_numpy().flatten() if pd.notna(x)]
+            self.set_instruments(instrument_list)
+            with open(self.jsonPath, "w", encoding="utf-8") as f:
+                json.dump(instrument_list, f, ensure_ascii=False, indent=2)
+
+        elif os.path.exists(self.jsonPath) and self.get_instruments == []:
+            with open(self.jsonPath, "r", encoding="utf-8") as f:
+                instrument_list = json.load(f)
+                self.set_instruments(instrument_list)
+        
+
+    def getTrackRange(self) -> List[Tuple[int,int]]:
+        if len(self.trackRange)!= 0:
+            return self.trackRange
+        col = self.pdFrame["system"]
+        groups = col.ne(col.shift()).cumsum()
+        ranges = (
+            self.pdFrame
+            .groupby(groups)
+            .apply(lambda g: (g.index[0], g.index[-1]))
+            .tolist()
+        )
+        self.trackRange = ranges
+        return ranges
+
+    def getGroupingRange(self)->List[List[Tuple[int,int]]]:
+        if len(self.groupingRange)!= 0:
+            return self.groupingRange
+        range1 = self.getTrackRange()
+
+        col2 = self.pdFrame["staffgroup"]
+        groups2 = col2.ne(col2.shift()).cumsum()
+        ranges2 = (
+            self.pdFrame
+            .groupby(groups2)
+            .apply(lambda g: (g.index[0], g.index[-1]))
+            .tolist()
+        )
+        rangeList = []
+        currentIdx = 0
+        for r in range1:
+            rangeInLine = []
+            while currentIdx < len(ranges2) and ranges2[currentIdx][1] <= r[1]:
+                rangeInLine.append(ranges2[currentIdx])
+                currentIdx += 1
+            rangeList.append(rangeInLine)
+        self.groupingRange = rangeList
+        return rangeList
+    def getInstrumentEachTrack(self) -> List[List[str]]:
+        return self.pdFrame[['combined1', 'combined2', 'combined3']].apply(
+            lambda row: row.dropna().tolist(),
+            axis=1
+        ).tolist()
+    def getInstrumentIndexEachTrack(self) -> List[List[int]]:
+        strList = self.getInstrumentEachTrack()
+        insList = self.get_instruments()
+        return [[insList.index(r) for r in row] for row in strList]
+    def getTrackForLineIdx(self, lineIdx: int) -> int:
+        for i, (start, end) in enumerate(self.trackRange):
+            if start <= lineIdx <= end:
+                return i
+        return -1
+
+    # static
+    @classmethod
+    def set_instruments(cls, instrumentList: List[str]):
+        cls.instruments = instrumentList
+
+
+    @classmethod
+    def add_instrument(cls, instrument: str):
+        cls.instruments.append(instrument)
+
+    @classmethod
+    def get_instruments(cls) -> List[str]:
+        return cls.instruments.copy()
+    
+def parseCsvData(csv_path: str):
+    with open(csv_path) as f:
+        csvData = list(csv.reader(f))
+    return csvData
+
 def constructBar(noteGroupMap:np.ndarray, 
-                   stemIdxMap: np.ndarray,
                    noteGroupVerticallyMerged: List[NoteGroup|None],
                    restMap: np.ndarray,
                    restList:List[Rest|None],
                    sfnClefMap:np.ndarray,
                    sfnClefList:List[Union[Accidentals,Clef, None]],
                    beamMapImg:np.ndarray,
-                   staffList:List[Staff]):
+                   staffList:List[Staff],
+                   pageMetadata:ScoreMetaData,
+                   barEachTrack:List[List[int]]):
     numLines = len(staffList)
     bb,gg,rr = cv2.split(beamMapImg)
     barList:List[List[Bar]] = [[] for _ in range(numLines)]
@@ -27,30 +128,23 @@ def constructBar(noteGroupMap:np.ndarray,
     listForMatching:List[List] = [None, noteGroupVerticallyMerged,sfnClefList, sfnClefList, None, restList]
     notFinishedBar = None
     allRanges = []
-    for kk in range(numLines):
-        allBars = []
-        sf0 = staffList[kk]
-        barPlace = np.unique(np.where(gg[sf0.ys[0]:sf0.ys[-1], sf0.left:]==4)[1])
-        isBar = np.sort(barPlace)
-        ranges = []
-        points = [0,isBar[0]] 
-        i = 1
-        while i < len(isBar):
-            while isBar[i] == isBar[i-1]+1 and i<len(isBar)-1:
-                i+=1
-            if i == len(isBar)-1 and isBar[i] == isBar[i-1]+1:
-                break
-            points.append(isBar[i])
-            i+=1
-        for i in range(len(points)-1):
-            if points[i+1]>points[i]+sf0.get_yOne():
-                ranges.append((points[i]+sf0.left, points[i+1]+sf0.left))
-        allRanges.append(ranges)
+    for barThisTrack in barEachTrack:
+        prevIdx = 0
+        rangeThisTrack = []
+        if len(barThisTrack) > 1:
+            while prevIdx < len(barThisTrack)-1:
+                if barThisTrack[prevIdx+1] - barThisTrack[prevIdx] > 5:
+                    rangeThisTrack.append((barThisTrack[prevIdx], barThisTrack[prevIdx+1]))
+                prevIdx += 1
+        allRanges.append(rangeThisTrack)
+
     for t in range(numLines): # trackNo
         printedLineNo = t # the 
         currBarList:List[Bar] = []
         sf0 = staffList[printedLineNo]
-        ranges = allRanges[t]
+        trackForThatLine = pageMetadata.getTrackForLineIdx(t)
+        ranges = allRanges[trackForThatLine]
+        isBar = barEachTrack[trackForThatLine]
         for ridx, rng in enumerate(ranges):
             if notFinishedBar is not None:
                 bar = notFinishedBar
@@ -146,100 +240,6 @@ def constructBar(noteGroupMap:np.ndarray,
         # in order of vln1's 1st line, 2nd line ... | vln2's 1st line, 2nd line ...
     return barList,allRanges, numBarsEachLine
 
-class ScoreMetaData:
-    # static 
-    instruments: List[str] = []
-
-    def __init__(self, pd_frame: pd.DataFrame, json_path: str, initInstruments = False):
-        self.jsonPath: str = json_path
-        self.pdFrame: pd.DataFrame = pd_frame
-        self.trackRange: List[Tuple[int,int]] = []
-        self.groupingRange: List[List[Tuple[int,int]]] = []
-        if initInstruments:
-            for i in range(1,4):
-                df[f"combined{i}"] = (
-                    df[f"ins{i}"]
-                    + df[f"part{i}"].apply(lambda x: "" if pd.isna(x) else '_'+str(int(x)))
-                    + df[f"tone{i}"].apply(lambda x: "" if pd.isna(x) else ':'+x)
-                )
-            df_filtered = df[['combined1', 'combined2','combined3']][df['system'] == 1]
-            instrument_list = [str(x) for x in df_filtered.to_numpy().flatten() if pd.notna(x)]
-            self.set_instruments(instrument_list)
-            with open(self.jsonPath, "w", encoding="utf-8") as f:
-                json.dump(instrument_list, f, ensure_ascii=False, indent=2)
-
-        elif os.path.exists(self.jsonPath) and self.get_instruments == []:
-            with open(self.jsonPath, "r", encoding="utf-8") as f:
-                instrument_list = json.load(f)
-                self.set_instruments(instrument_list)
-        
-
-    def getTrackRange(self) -> List[Tuple[int,int]]:
-        if len(self.trackRange)!= 0:
-            return self.trackRange
-        col = self.pdFrame["system"]
-        groups = col.ne(col.shift()).cumsum()
-        ranges = (
-            self.pdFrame
-            .groupby(groups)
-            .apply(lambda g: (g.index[0], g.index[-1]))
-            .tolist()
-        )
-        self.trackRange = ranges
-        return ranges
-
-    def getGroupingRange(self)->List[List[Tuple[int,int]]]:
-        if len(self.groupingRange)!= 0:
-            return self.groupingRange
-        range1 = self.getTrackRange()
-
-        col2 = self.pdFrame["staffgroup"]
-        groups2 = col2.ne(col2.shift()).cumsum()
-        ranges2 = (
-            self.pdFrame
-            .groupby(groups2)
-            .apply(lambda g: (g.index[0], g.index[-1]))
-            .tolist()
-        )
-        rangeList = []
-        currentIdx = 0
-        for r in range1:
-            rangeInLine = []
-            while currentIdx < len(ranges2) and ranges2[currentIdx][1] <= r[1]:
-                rangeInLine.append(ranges2[currentIdx])
-                currentIdx += 1
-            rangeList.append(rangeInLine)
-        self.groupingRange = rangeList
-        return rangeList
-    def getInstrumentEachTrack(self) -> List[List[str]]:
-        return self.pdFrame[['combined1', 'combined2', 'combined3']].apply(
-            lambda row: row.dropna().tolist(),
-            axis=1
-        ).tolist()
-    def getInstrumentIndexEachTrack(self) -> List[List[int]]:
-        strList = self.getInstrumentEachTrack()
-        insList = self.get_instruments()
-        return [[insList.index(r) for r in row] for row in strList]
-
-
-    # static
-    @classmethod
-    def set_instruments(cls, instrumentList: List[str]):
-        cls.instruments = instrumentList
-
-
-    @classmethod
-    def add_instrument(cls, instrument: str):
-        cls.instruments.append(instrument)
-
-    @classmethod
-    def get_instruments(cls) -> List[str]:
-        return cls.instruments.copy()
-    
-def parseCsvData(csv_path: str):
-    with open(csv_path) as f:
-        csvData = list(csv.reader(f))
-    
 
 if __name__ == '__main__':
     noteGroupMap: np.ndarray
@@ -285,8 +285,9 @@ if __name__ == '__main__':
         barSum = np.sum(staffImgBinary[trkRange[0]:trkRange[1],:],axis=0)
         barPos = np.where(barSum>(trkRange[1]-trkRange[0])*0.5)
         img[staffStart:staffEnd, barPos] = (0,0,255)
-        barEachTrack.append(barPos)
-        
+        barEachTrack += barPos
+    barList,allRanges, numBarsEachLine = constructBar(noteGroupMap, noteGroupVerticallyMerged, restMap ,restList, sfnClefMap, sfnClefList, beamMapImg, staffList, pageMetadata, barEachTrack)
+
     # get Barline locations -> bar center for each track: List[List[int]]
     # get the list of 
     print()
